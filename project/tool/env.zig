d67263d77e20624b3ef74539029314c2a3ad2aa0
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2024-03-04 15:19:48 -0500

    Remove Zig

--- project/tool/env.zig
+++
@@ -1,4 +0,0 @@
-const std = @import("std");
-const target = @import("builtin").target;
-
-pub const isWindows = target.os.tag == .windows;
--- project/tool/string.zig
+++
@@ -1,7 +0,0 @@
-const std = @import("std");
-const Env = @import("env.zig");
-
-pub const char = if (Env.isWindows) u16 else u8;
-pub const string = []const char;
-pub const stringZ = [:0]const char;
-pub const mut_string = []char;
