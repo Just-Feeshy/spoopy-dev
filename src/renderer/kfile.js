45d7c2cb48205e624b2ca3a415f9b8f053d9375f
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2025-08-04 06:10:52 -0400

    New

---
+++ src/renderer/kfile.js
@@ -0,0 +1,26 @@
+// Due to compiling this into a json,
+// at of config will be missing what it seems as
+// critical components, but it works fine since it's all
+// handled via our main meson build system.
+
+// TODO (Framework): Have kfile be generated, instead of hardcoded
+// Plus, have it be regenerated or written to if characters don't match
+// or something like that
+
+const project = new Project('spoopy_renderer');
+await project.addProject(findKore());
+
+let target_platform = platform;
+if (target_platform === Platform.OSX) {
+    target_platform = 'macos';
+}
+
+project.addIncludeDir('../../include');
+project.addIncludeDir('Kore/backends/system/' + target_platform + '/includes/kore3/backend');
+
+project.addFile('src/**');
+project.addKongDir('shaders');
+project.addExclude('**.cpp');
+
+project.flatten();
+resolve(project);
--- src/renderer/src/spoopy_video.c
+++ src/renderer/src/spoopy_video.c
@@ -2,58 +2,9 @@
 #include <spoopy_video.h>
 #include <kore3/gpu/device.h>
 #include <kore3/system.h>
-#include <kong.h>
-
-// Needs to be in a RHI
-static kore_gpu_device device;
-static kore_gpu_command_list command_list;
-
-// Needs to be managed better
-static vertex_in_buffer vertices;
-static kore_gpu_buffer indices;
-
 
 void spoopy_video_init(const spoopy_video_init_params_t* params) {
     kore_init(params->title, params->width, params->height, NULL, NULL);
-
-    kore_gpu_device_wishlist wishlist = {0};
-    kore_gpu_device_create(&device, &wishlist);
-
-    kong_init(&device);
-
-    kore_gpu_device_create_command_list(&device, KORE_GPU_COMMAND_LIST_TYPE_GRAPHICS, &command_list);
-
-    kong_create_buffer_vertex_in(&device, 3, &vertices);
-    vertex_in *v = kong_vertex_in_buffer_lock(&vertices);
-
-    v[0].pos.x = -1.0f;
-	v[0].pos.y = -1.0f;
-	v[0].pos.z = 0.5f;
-
-	v[1].pos.x = 1.0f;
-	v[1].pos.y = -1.0f;
-	v[1].pos.z = 0.5f;
-
-	v[2].pos.x = -1.0f;
-	v[2].pos.y = 1.0f;
-	v[2].pos.z = 0.5f;
-
-    kong_vertex_in_buffer_unlock(&vertices);
-
-    kore_gpu_buffer_parameters params = {
-	    .size        = 3 * sizeof(uint16_t),
-	    .usage_flags = KORE_GPU_BUFFER_USAGE_INDEX | KORE_GPU_BUFFER_USAGE_CPU_WRITE,
-	};
-	kore_gpu_device_create_buffer(&device, &params, &indices);
-	{
-		uint16_t *i = (uint16_t *)kore_gpu_buffer_lock_all(&indices);
-
-		i[0] = 0;
-		i[1] = 1;
-		i[2] = 2;
-
-		kore_gpu_buffer_unlock(&indices);
-	}
 }
 
 void spoopy_video_shutdown(void) {
