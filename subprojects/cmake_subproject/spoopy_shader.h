7fa3b9bbc07bf2a3587f37491d2aba41e58961f2
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2025-08-13 01:38:37 -0400

    New CMake

---
+++ subprojects/cmake_subproject/spoopy_shader.h
@@ -0,0 +1,19 @@
+#ifndef SPOOPY_SPOOPY_SHADER_H
+#define SPOOPY_SPOOPY_SHADER_H
+
+// #include <spoopy.h>
+// #include <shaderlib/defs.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// typedef SlangResult spoopy_shader_result_t;
+
+void spoopy_shader_init(void);
+
+#ifdef __cplusplus
+}
+#endif // extern "C"
+
+#endif // SPOOPY_SPOOPY_SHADER_H
